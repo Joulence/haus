d52d43a9a13ec1d70a4b210acb82ac66ec53b820
Joulence <4erviken@gmail.com>
2023-03-18 01:02:22 +0200

    header mobile adaptation

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -1,24 +1,46 @@
-export function onclick() {
-    let hamMenu = document.getElementById("hamMenu");
-    let navMenu = document.getElementById("nav")
-    hamMenu.addEventListener("click", btnPressed);
-    hamMenu.addEventListener("click", showMenu);
+export const onclick = () => {
+  let nav = document.getElementById('header-nav');
+  let hamMenu = document.getElementById('hamMenu');
+  let navMenu = document.getElementById('navMenu');
+  let navMenu__2 = document.getElementById('navMenu2');
 
-    function btnPressed() {
-        let isHavMenu = (hamMenu.classList.length > 1) ?
-        hamMenu.classList.remove("is-open") :
-        hamMenu.classList.add("is-open");
-        return isHavMenu;
-      }
+  let change = false;
 
-      function showMenu() {
-        let x = document.getElementById("myLinks");
-        if (x.style.display === "block") {
-          x.style.display = "none";
-          navMenu.style.background = "";
-        } else {
-          x.style.display = "block";
-          navMenu.style.background = "#fff";
-        }
+  const addStyles = () => {
+    nav.classList.add('nav-transform');
+    navMenu.classList.add('menu-show');
+    navMenu__2.classList.add('menu-show');
+  }
+
+  const removeStyles = () => {
+    nav.classList.remove('nav-transform');
+    navMenu.classList.remove('menu-show');
+    navMenu__2.classList.remove('menu-show');
+  }
+
+  const ifWidth = innerWidth <= 820;
+
+  const adapt = () => {
+    if (ifWidth) {
+      console.log(innerWidth);
+      if (!navMenu.classList.contains('menu-show')) {
+        addStyles();
+      } else {
+        removeStyles();
       }
+    } else {
+      console.log(false);
+      removeStyles();
+    }
+  }
+
+  window.addEventListener('resize', () => {
+    if (innerWidth > 820) {
+      removeStyles();
+    }
+  });
+
+  hamMenu.addEventListener('click', () => {
+    adapt();
+  });
 }
